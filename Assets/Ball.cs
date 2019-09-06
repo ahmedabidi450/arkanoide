bfff55f2334dc8b6646a6ec49517d53c4631d09e
ahmedabidi450 <54631055+ahmedabidi450@users.noreply.github.com>
2019-09-06 09:57:09 +0100

    Add files via upload

---
+++ Assets/Ball.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 25;
+    float HitFactor(Vector2 BallPosition, Vector2 RacketPosition, float RacketHeight)
+    {
+        return (BallPosition.x - RacketPosition.x) / RacketHeight;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Racket")
+        {
+            float x = HitFactor(transform.position,
+                collision.transform.position,
+                collision.collider.bounds.size.x);
+            Vector2 direction = new Vector2(x, 1).normalized;
+            GetComponent<Rigidbody2D>().velocity = direction * speed;
+        }
+    }
+
+
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/mouvement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouvement : MonoBehaviour
+{
+    public float speed = 30;
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * horizontal * speed;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
